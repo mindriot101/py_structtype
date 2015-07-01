848e84d62dd9173a8411b3d0172f1da7fbba2fbd
Simon Walker <s.r.walker101@googlemail.com>
2015-07-01 13:06:02 +0100

    Add initialised variables

--- structtype.py
+++ structtype.py
@@ -1,2 +1,7 @@
 def Struct(*names):
-    return type('Struct', (object, ), {})
+    variables_dict = {}
+    for name in names:
+        variables_dict[name] = None
+
+    full_dict = variables_dict
+    return type('Struct', (object, ), full_dict)
--- testing/test_structtype.py
+++ testing/test_structtype.py
@@ -1,6 +1,22 @@
 import inspect
 from structtype import Struct
+import pytest
 
-def test_stuct_instance_is_class():
-    s = Struct("a", "b")
-    assert inspect.isclass(s)
+
+@pytest.fixture
+def struct():
+    return Struct("a", "b")
+
+
+def test_stuct_instance_is_class(struct):
+    assert inspect.isclass(struct)
+
+
+def test_instance_values_initialised_to_None(struct):
+
+    class SubClass(struct):
+        pass
+
+    s = SubClass()
+
+    assert s.a is None and s.b is None
