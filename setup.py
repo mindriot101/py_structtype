a02189da3be805aae07ef46cc291bb29df886476
Simon Walker <s.r.walker101@googlemail.com>
2015-07-01 13:16:13 +0100

    Extra setup parameters

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup
 setup(
         name='structtype',
         py_modules=['structtype'],
+        author='Simon Walker',
+        author_email='s.r.walker101@googlemail.com',
+        version='0.0.1',
 )
 
